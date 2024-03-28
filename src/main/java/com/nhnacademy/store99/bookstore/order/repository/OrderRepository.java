5125ee5cbb456c4a0193b1ec798a4411de7c8df7
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-03-28 12:39:42 +0900

    {update}: order 기능 추가- OrderRepository 생성

---
+++ src/main/java/com/nhnacademy/store99/bookstore/order/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.store99.bookstore.order.repository;
+
+import com.nhnacademy.store99.bookstore.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
+}
