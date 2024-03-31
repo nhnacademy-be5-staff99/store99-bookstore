cb840d5e4ef7026ae3ae576715630f95aa279175
yrrho2 <dyrho22@naver.com>
2024-03-31 15:57:29 +0900

    rename: BookInterface -> BookRepositoryInterface

--- src/main/java/com/nhnacademy/store99/bookstore/book/repository/BookRepositoryInterface.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book/repository/BookRepositoryInterface.java
@@ -3,5 +3,5 @@ package com.nhnacademy.store99.bookstore.book.repository;
 import com.nhnacademy.store99.bookstore.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookInterface extends JpaRepository<Book, Long> {
+public interface BookRepositoryInterface extends JpaRepository<Book, Long> {
 }
