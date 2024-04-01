c26d7de3258915079f7b5ef1b4fb07730d935e0e
yrrho2 <dyrho22@naver.com>
2024-04-01 16:03:17 +0900

    feat: responseEntity로 반환. 도서 get api 정상작동

--- src/main/java/com/nhnacademy/store99/bookstore/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book/controller/BookController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.store99.bookstore.book.service.BookServiceInterface;
 import com.nhnacademy.store99.bookstore.common.response.CommonHeader;
 import com.nhnacademy.store99.bookstore.common.response.CommonResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,26 +23,28 @@ public class BookController {
     }
 
     @GetMapping("/{bookId}")
-    public CommonResponse getBook(@PathVariable("bookId") Long id) {
+    public ResponseEntity<CommonResponse> getBook(@PathVariable("bookId") Long id) {
         CommonHeader commonHeader = CommonHeader.builder()
                 .httpStatus(HttpStatus.OK)
                 .build();
-        return CommonResponse.builder()
+        CommonResponse commonResponse = CommonResponse.builder()
                 .header(commonHeader)
                 .result(bookService.getBook(id).get())
                 .build();
+        return ResponseEntity.ok(commonResponse);
 
 
     }
 
     @PostMapping
-    public CommonResponse postBook(@RequestBody BookDTO bookDTO) {
+    public ResponseEntity<CommonResponse> postBook(@RequestBody BookDTO bookDTO) {
         CommonHeader commonHeader = CommonHeader.builder()
                 .httpStatus(HttpStatus.OK).build();
-        return CommonResponse.builder()
+        CommonResponse commonResponse = CommonResponse.builder()
                 .header(commonHeader)
                 .result(bookService.postBook(bookDTO))
                 .build();
+        return ResponseEntity.ok(commonResponse);
     }
 
 }
