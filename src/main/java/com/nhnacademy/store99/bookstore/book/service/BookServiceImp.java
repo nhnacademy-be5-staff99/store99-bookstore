421d43e21adc5fedaa8ab5aed21ffeb986fc208f
yrrho2 <dyrho22@naver.com>
2024-04-02 17:54:50 +0900

    feat: getBook 테스트 성공

--- src/main/java/com/nhnacademy/store99/bookstore/book/service/BookServiceImp.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book/service/BookServiceImp.java
@@ -2,16 +2,16 @@ package com.nhnacademy.store99.bookstore.book.service;
 
 import com.nhnacademy.store99.bookstore.book.entity.Book;
 import com.nhnacademy.store99.bookstore.book.entity.BookRequest;
-import com.nhnacademy.store99.bookstore.book.repository.BookRepositoryInterface;
+import com.nhnacademy.store99.bookstore.book.repository.BookRepository;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImp implements BookServiceInterface {
-    final private BookRepositoryInterface bookRepository;
+    final private BookRepository bookRepository;
 
-    public BookServiceImp(BookRepositoryInterface bookRepositoryInterface) {
-        this.bookRepository = bookRepositoryInterface;
+    public BookServiceImp(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
     @Override
