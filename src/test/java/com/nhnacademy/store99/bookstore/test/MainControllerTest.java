3243803f3cde6e3d58bc9e6d663c9c96f2f760d9
김승규 <rlatmdrb1996@gmail.com>
2024-04-04 00:24:19 +0900

    test: spring rest docs 테스트

---
+++ src/test/java/com/nhnacademy/store99/bookstore/test/MainControllerTest.java
@@ -0,0 +1,39 @@
+package com.nhnacademy.store99.bookstore.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MainController.class)
+@ExtendWith(RestDocumentationExtension.class)
+public class MainControllerTest {
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext,
+                      RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+
+    @Test
+    public void getTestTest() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/test").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("get-test"));
+    }
+
+}
