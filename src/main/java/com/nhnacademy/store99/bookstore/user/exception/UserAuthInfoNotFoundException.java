034aab6d5704106b40b464899d62316db80a3f44
Song Ahyeon <ahyeonsong2001@gmail.com>
2024-04-08 02:30:56 +0900

    add: user 정보를 찾지 못한 경우에 대한 예외 처리 코드 추가

--- src/main/java/com/nhnacademy/store99/bookstore/user/exception/UserAuthInfoNotFoundException.java
+++ src/main/java/com/nhnacademy/store99/bookstore/user/exception/UserAuthInfoNotFoundException.java
@@ -1,6 +1,6 @@
 package com.nhnacademy.store99.bookstore.user.exception;
 
-public class UserAuthInfoNotFoundException extends RuntimeException{
+public class UserAuthInfoNotFoundException extends RuntimeException {
     public UserAuthInfoNotFoundException(String message) {
         super(message);
     }
