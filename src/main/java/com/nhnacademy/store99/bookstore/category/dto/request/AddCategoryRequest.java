68e8fc5dab1c686b87315f11c91857e7562afd37
김승규 <rlatmdrb1996@gmail.com>
2024-04-13 16:24:32 +0900

    feat: validation 추가
    Resolves: #50

--- src/main/java/com/nhnacademy/store99/bookstore/category/dto/request/AddCategoryRequest.java
+++ src/main/java/com/nhnacademy/store99/bookstore/category/dto/request/AddCategoryRequest.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.store99.bookstore.category.dto.request;
 
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddCategoryRequest {
+    @NotNull
     private String categoryName;
 
     private Integer categoryDepth;
