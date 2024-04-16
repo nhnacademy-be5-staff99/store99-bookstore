7eef49e159349ca23c662b76288d82b9d7d0be77
yrrho2 <dyrho22@naver.com>
2024-04-16 11:16:35 +0900

    fix: 에러수정

--- src/main/java/com/nhnacademy/store99/bookstore/book_author/controller/BookAuthorController.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book_author/controller/BookAuthorController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,12 +31,12 @@ public class BookAuthorController {
     }
 
 
-    @GetMapping("/book/{bookId}")
-    public ResponseEntity<CommonResponse> getBookAuthor(@PathVariable("bookId") Long id) {
+    @GetMapping("/book")
+    public ResponseEntity<CommonResponse> getBookAuthor(@RequestParam("bookId") Long bookId) {
         CommonHeader commonHeader = CommonHeader.builder().httpStatus(HttpStatus.OK).build();
         CommonResponse commonResponse = CommonResponse.builder()
                 .header(commonHeader)
-                .result(bookAuthorService.getAuthorBook(id))
+                .result(bookAuthorService.getAuthorBook(bookId))
                 .build();
         return ResponseEntity.ok(commonResponse);
     }
