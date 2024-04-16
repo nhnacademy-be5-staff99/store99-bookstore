0820a7526550f6e222704ca26aca64e668683ac7
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-04-16 10:52:58 +0900

    {feature}:controller, Test 설정

---
+++ src/test/java/com/nhnacademy/store99/bookstore/like/repository/LikeRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.store99.bookstore.like.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class LikeRepositoryTest {
+    
+
+}
