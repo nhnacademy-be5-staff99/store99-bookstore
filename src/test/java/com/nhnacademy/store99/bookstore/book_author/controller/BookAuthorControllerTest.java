e8b7876df0e4031266feb34578b9f185f438a5dd
yrrho2 <dyrho22@naver.com>
2024-04-16 17:52:52 +0900

    feat: 도서-저자 테스트작성 중간저장

--- src/test/java/com/nhnacademy/store99/bookstore/book_author/controller/BookAuthorControllerTest.java
+++ src/test/java/com/nhnacademy/store99/bookstore/book_author/controller/BookAuthorControllerTest.java
@@ -1,5 +1,27 @@
 package com.nhnacademy.store99.bookstore.book_author.controller;
 
+import com.nhnacademy.store99.bookstore.book_author.service.BookAuthorService;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(BookAuthorController.class)
 class BookAuthorControllerTest {
 
+    private BookAuthorService bookAuthorService;
+
+    @Test
+    void TestGetBookAuthorbyAuthorId() {
+        // given
+        Long authorId = Mockito.anyLong();
+        BDDMockito.given(bookAuthorService.getAuthorByAuthorId(authorId)).willReturn(Mockito.anyList());
+
+
+    }
+
+    @Test
+    void TestGetBookAuthor() {
+
+    }
 }
