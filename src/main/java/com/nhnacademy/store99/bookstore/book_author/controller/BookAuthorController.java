db4b9b80d465bf90e77bfa812d86026a822790a2
yrrho2 <dyrho22@naver.com>
2024-04-16 17:10:03 +0900

    comment: jdoc 작성

--- src/main/java/com/nhnacademy/store99/bookstore/book_author/controller/BookAuthorController.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book_author/controller/BookAuthorController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 도서의 작가를 참조하는 컨트롤러입니다.
+ *
+ * @author yrrho2
+ */
 @RestController
 @RequestMapping("/open/v1/books/author")
 public class BookAuthorController {
@@ -20,17 +25,29 @@ public class BookAuthorController {
         this.bookAuthorService = bookAuthorService;
     }
 
+    /**
+     * 작가가 작성한 책들을 반환합니다.
+     *
+     * @param authorId 작가의 id
+     * @return 검색된 도서들의 List를 반환합니다.
+     */
     @GetMapping("/{authorId}")
-    public ResponseEntity<CommonResponse> getBookAuthorbyAuthorId(@PathVariable("authorId") Long id) {
+    public ResponseEntity<CommonResponse> getBookAuthorbyAuthorId(@PathVariable("authorId") Long authorId) {
         CommonHeader commonHeader = CommonHeader.builder().httpStatus(HttpStatus.OK).build();
         CommonResponse commonResponse = CommonResponse.builder()
                 .header(commonHeader)
-                .result(bookAuthorService.getAuthorByAuthorId(id))
+                .result(bookAuthorService.getAuthorByAuthorId(authorId))
                 .build();
         return ResponseEntity.ok(commonResponse);
     }
 
 
+    /**
+     * 도서 id를 받아 작가를 참조합니다
+     *
+     * @param bookId 도서Id
+     * @return 검색된 작가의 id가 담긴 BookAuthorResponse를 반환합니다
+     */
     @GetMapping("/book")
     public ResponseEntity<CommonResponse> getBookAuthor(@RequestParam("bookId") Long bookId) {
         CommonHeader commonHeader = CommonHeader.builder().httpStatus(HttpStatus.OK).build();
--- src/main/java/com/nhnacademy/store99/bookstore/book_author/service/BookAuthorServiceImp.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book_author/service/BookAuthorServiceImp.java
@@ -7,6 +7,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 도서-작가 참조 기능을 제공하는 서비스 구현체
+ *
+ * @author yrrho2
+ */
 @Transactional(readOnly = true)
 @Service
 public class BookAuthorServiceImp implements BookAuthorService {
@@ -16,9 +21,15 @@ public class BookAuthorServiceImp implements BookAuthorService {
         this.bookAuthorRepository = bookAuthorRepository;
     }
 
+    /**
+     * 도서의 id를 사용하여 작가 검색
+     *
+     * @param bookId 도서id
+     * @return 도서id, 작가id, 도서정보를 반환
+     */
     @Override
-    public BookAuthorResponse getAuthorBook(Long id) {
-        BookAuthor bookAuthor = bookAuthorRepository.findById(id).get();
+    public BookAuthorResponse getAuthorBook(Long bookId) {
+        BookAuthor bookAuthor = bookAuthorRepository.findById(bookId).get();
         return BookAuthorResponse.builder()
                 .authorName(bookAuthor.getAuthor().getAuthorName())
                 .authorId(bookAuthor.getAuthor().getId())
@@ -26,8 +37,14 @@ public class BookAuthorServiceImp implements BookAuthorService {
                 .build();
     }
 
+    /**
+     * 작가의 id를 사용하여 도서들을 조회합니다
+     *
+     * @param authorId 작가 id
+     * @return BookAuthor가 담긴 리스트를 반환합니다.
+     */
     @Override
-    public List<BookAuthor> getAuthorByAuthorId(Long id) {
-        return bookAuthorRepository.findBookAuthorsByAuthorId(id);
+    public List<BookAuthor> getAuthorByAuthorId(Long authorId) {
+        return bookAuthorRepository.findBookAuthorsByAuthorId(authorId);
     }
 }
