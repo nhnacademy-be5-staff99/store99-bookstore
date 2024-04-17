d5688cf4429afa54f737681f36d3fe38826e23ec
김승규 <rlatmdrb1996@gmail.com>
2024-04-17 15:57:31 +0900

    feat: 관리자 카테고리 조회 기능 개발

--- src/main/java/com/nhnacademy/store99/bookstore/category/dto/response/CategoryForAdminResponse.java
+++ src/main/java/com/nhnacademy/store99/bookstore/category/dto/response/CategoryForAdminResponse.java
@@ -4,18 +4,16 @@ import com.nhnacademy.store99.bookstore.category.entity.Category;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 /**
  * 카테고리 조회 응답 DTO
  *
  * @author seunggyu-kim
  */
-@ToString
 @Getter
-@NoArgsConstructor
+@Builder
 @AllArgsConstructor
 public class CategoryForAdminResponse {
     private Long id;
@@ -29,12 +27,13 @@ public class CategoryForAdminResponse {
     private LocalDateTime deletedAt;
 
     public static CategoryForAdminResponse from(Category category) {
-        return new CategoryForAdminResponse(
-                category.getId(),
-                category.getCategoryName(),
-                category.getCategoryDepth(),
-                Objects.nonNull(category.getParentCategory()) ? category.getParentCategory().getId() : null,
-                category.getDeletedAt()
-        );
+        return CategoryForAdminResponse.builder()
+                .id(category.getId())
+                .categoryName(category.getCategoryName())
+                .categoryDepth(category.getCategoryDepth())
+                .parentCategoryId(
+                        Objects.nonNull(category.getParentCategory()) ? category.getParentCategory().getId() : null)
+                .deletedAt(category.getDeletedAt())
+                .build();
     }
 }
--- src/test/java/com/nhnacademy/store99/bookstore/category/service/impl/CategoryAdminServiceImplTest.java
+++ src/test/java/com/nhnacademy/store99/bookstore/category/service/impl/CategoryAdminServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.nhnacademy.store99.bookstore.category.service.impl;
 
 import com.nhnacademy.store99.bookstore.category.dto.request.AddCategoryRequest;
+import com.nhnacademy.store99.bookstore.category.dto.response.CategoryForAdminResponse;
 import com.nhnacademy.store99.bookstore.category.entity.Category;
 import com.nhnacademy.store99.bookstore.category.exception.CategoryNotFoundException;
 import com.nhnacademy.store99.bookstore.category.repository.CategoryRepository;
+import java.util.List;
 import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +16,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 /**
  * @author seunggyu-kim
@@ -28,7 +33,43 @@ class CategoryAdminServiceImplTest {
 
     @Test
     void getCategories() {
-        // TODO
+        // given
+        PageRequest pageable = PageRequest.of(0, 10);
+        List<Category> cateogryList = List.of(
+                Category.builder().id(1L).categoryName("국내도서").categoryDepth(1).build(),
+                Category.builder().id(2L).categoryName("과학").categoryDepth(2).parentCategory(Category.builder().id(1L).categoryName("국내도서").categoryDepth(1).build()).build(),
+                Category.builder().id(3L).categoryName("컴퓨터/모바일").categoryDepth(2).parentCategory(Category.builder().id(1L).categoryName("국내도서").categoryDepth(1).build()).build(),
+                Category.builder().id(4L).categoryName("경제경영").categoryDepth(2).parentCategory(Category.builder().id(1L).categoryName("국내도서").categoryDepth(1).build()).build(),
+                Category.builder().id(5L).categoryName("대학교재/전문서적").categoryDepth(2).parentCategory(Category.builder().id(1L).categoryName("국내도서").categoryDepth(1).build()).build(),
+                Category.builder().id(6L).categoryName("프로그래밍 개발/방법론").categoryDepth(3).parentCategory(Category.builder().id(3L).categoryName("컴퓨터/모바일").categoryDepth(2).build()).build(),
+                Category.builder().id(7L).categoryName("인공지능").categoryDepth(3).parentCategory(Category.builder().id(3L).categoryName("컴퓨터/모바일").categoryDepth(2).build()).build(),
+                Category.builder().id(8L).categoryName("컴퓨터 공학").categoryDepth(3).parentCategory(Category.builder().id(3L).categoryName("컴퓨터/모바일").categoryDepth(2).build()).build(),
+                Category.builder().id(9L).categoryName("프로그래밍 언어").categoryDepth(3).parentCategory(Category.builder().id(3L).categoryName("컴퓨터/모바일").categoryDepth(2).build()).build(),
+                Category.builder().id(10L).categoryName("기초과학/교양과학").categoryDepth(3).parentCategory(Category.builder().id(2L).categoryName("과학").categoryDepth(2).build()).build()
+        );
+        Page<Category> categoryPage = new PageImpl<>(cateogryList);
+
+        List<CategoryForAdminResponse> categoCategoryForAdminResponseList = List.of(
+                CategoryForAdminResponse.builder().id(1L).categoryName("국내도서").categoryDepth(1).build(),
+                CategoryForAdminResponse.builder().id(2L).categoryName("과학").categoryDepth(2).parentCategoryId(1L).build(),
+                CategoryForAdminResponse.builder().id(3L).categoryName("컴퓨터/모바일").categoryDepth(2).parentCategoryId(1L).build(),
+                CategoryForAdminResponse.builder().id(4L).categoryName("경제경영").categoryDepth(2).parentCategoryId(1L).build(),
+                CategoryForAdminResponse.builder().id(5L).categoryName("대학교재/전문서적").categoryDepth(2).parentCategoryId(1L).build(),
+                CategoryForAdminResponse.builder().id(6L).categoryName("프로그래밍 개발/방법론").categoryDepth(3).parentCategoryId(3L).build(),
+                CategoryForAdminResponse.builder().id(7L).categoryName("인공지능").categoryDepth(3).parentCategoryId(3L).build(),
+                CategoryForAdminResponse.builder().id(8L).categoryName("컴퓨터 공학").categoryDepth(3).parentCategoryId(3L).build(),
+                CategoryForAdminResponse.builder().id(9L).categoryName("프로그래밍 언어").categoryDepth(3).parentCategoryId(3L).build(),
+                CategoryForAdminResponse.builder().id(10L).categoryName("기초과학/교양과학").categoryDepth(3).parentCategoryId(2L).build()
+        );
+        Page<CategoryForAdminResponse> expectedCategoryPage = new PageImpl<>(categoCategoryForAdminResponseList);
+
+        BDDMockito.given(categoryRepository.findAll(pageable)).willReturn(categoryPage);
+
+        // when
+        Page<CategoryForAdminResponse> actualCategoryPage = categoryAdminService.getCategories(pageable);
+
+        // then
+        Assertions.assertThat(actualCategoryPage).usingRecursiveComparison().isEqualTo(expectedCategoryPage);
     }
 
     @DisplayName("카테고리 추가 성공")
