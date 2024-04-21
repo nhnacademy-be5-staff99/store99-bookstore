cc92f29280761cba66a41519cb687800503091ac
김승규 <rlatmdrb1996@gmail.com>
2024-04-22 01:36:22 +0900

    refactor: LinkedList 사용으로 최적화
    반복된 추가가 일어나기에 LinkedList 사용

--- src/main/java/com/nhnacademy/store99/bookstore/category/dto/response/CategoryChildrenListAndRouteResponse.java
+++ src/main/java/com/nhnacademy/store99/bookstore/category/dto/response/CategoryChildrenListAndRouteResponse.java
@@ -2,7 +2,7 @@ package com.nhnacademy.store99.bookstore.category.dto.response;
 
 import com.nhnacademy.store99.bookstore.category.dto.ActiveCategoryIdAndNameDto;
 import com.nhnacademy.store99.bookstore.category.entity.Category;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +15,7 @@ public class CategoryChildrenListAndRouteResponse {
     /**
      * 현재 검색된 카테고리까지의 경로를 역순으로 반환합니다.
      */
-    public List<ActiveCategoryIdAndNameDto> nowCategoryRoute;
+    public LinkedList<ActiveCategoryIdAndNameDto> nowCategoryRoute;
 
     /**
      * 현재 카테고리의 자식 카테고리 목록을 반환합니다.
@@ -24,11 +24,10 @@ public class CategoryChildrenListAndRouteResponse {
     public List<ActiveCategoryIdAndNameDto> childrenCategoryList;
 
     public void setNowCategoryRouteByCategory(Category category) {
-        nowCategoryRoute = new ArrayList<>();
-        nowCategoryRoute.add(new ActiveCategoryIdAndNameDto(category.getId(), category.getCategoryName()));
-        while(category.getParentCategory() != null) {
+        nowCategoryRoute = new LinkedList<>();
+        while(category != null) {
+            nowCategoryRoute.addFirst(new ActiveCategoryIdAndNameDto(category.getId(), category.getCategoryName()));
             category = category.getParentCategory();
-            nowCategoryRoute.add(new ActiveCategoryIdAndNameDto(category.getId(), category.getCategoryName()));
         }
     }
 }
