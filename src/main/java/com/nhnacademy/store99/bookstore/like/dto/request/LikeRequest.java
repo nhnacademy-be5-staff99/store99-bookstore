706c887ee2ec2fe34374a4d2aedb38d02ef11c6a
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-04-22 21:28:19 +0900

    {add}: Like 기능 추가

--- src/main/java/com/nhnacademy/store99/bookstore/like/dto/request/LikeRequest.java
+++ src/main/java/com/nhnacademy/store99/bookstore/like/dto/request/LikeRequest.java
@@ -1,7 +1,5 @@
 package com.nhnacademy.store99.bookstore.like.dto.request;
 
-import com.nhnacademy.store99.bookstore.book.entity.Book;
-import com.nhnacademy.store99.bookstore.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,7 +7,8 @@ import lombok.Data;
 @AllArgsConstructor
 public class LikeRequest {
 
-    private Book book;
+    private Long bookId;
 
-    private User user;
+    private Long userId;
 }
+
