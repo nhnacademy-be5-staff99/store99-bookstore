b2bca826983245e3b0f1013e3a0f747aba383a54
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-04-25 01:45:13 +0900

    {add}: addLike api 생성

--- src/main/java/com/nhnacademy/store99/bookstore/like/controller/LikeController.java
+++ src/main/java/com/nhnacademy/store99/bookstore/like/controller/LikeController.java
@@ -1,15 +1,14 @@
 package com.nhnacademy.store99.bookstore.like.controller;
 
+import com.nhnacademy.store99.bookstore.common.response.CommonHeader;
 import com.nhnacademy.store99.bookstore.common.response.CommonResponse;
 import com.nhnacademy.store99.bookstore.like.dto.request.LikeRequest;
 import com.nhnacademy.store99.bookstore.like.service.LikeService;
-import java.net.URI;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,11 +26,18 @@ public class LikeController {
     }
 
     @PostMapping("/v1/likes")
-    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<CommonResponse<Void>> postAddLike(@RequestBody @Valid LikeRequest req) {
-
         likeService.addLike(req);
-        return ResponseEntity.created(URI.create("/v1/likes")).build();
+        CommonHeader commonHeader = CommonHeader.builder()
+                .httpStatus(HttpStatus.CREATED)
+                .resultMessage("Successfully add like")
+                .build();
+        CommonResponse<Void> commonResponse = CommonResponse.<Void>builder()
+                .header(commonHeader)
+                .build();
+        return ResponseEntity.ok(commonResponse);
+
     }
+
 
 }
