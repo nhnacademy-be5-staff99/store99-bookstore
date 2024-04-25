d49e58d22858c7cb63b23b943273cd95d5178891
yrrho2 <dyrho22@naver.com>
2024-04-25 17:55:20 +0900

    add: BookImageDTO, 레포지토리, 서비스 메소드 생성

--- src/main/java/com/nhnacademy/store99/bookstore/book_image/repository/impl/BookImageRepositoryImpl.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book_image/repository/impl/BookImageRepositoryImpl.java
@@ -1,6 +1,11 @@
 package com.nhnacademy.store99.bookstore.book_image.repository.impl;
 
 import com.nhnacademy.store99.bookstore.book_image.repository.BookImageRepository;
+import com.nhnacademy.store99.bookstore.book_image.response.BookImageDTO;
 
 public class BookImageRepositoryImpl implements BookImageRepository {
+    @Override
+    public BookImageDTO getBookImageData(Long bookId) {
+        return null;
+    }
 }
--- src/main/java/com/nhnacademy/store99/bookstore/book_image/service/impl/BookImageServiceImpl.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book_image/service/impl/BookImageServiceImpl.java
@@ -1,6 +1,11 @@
 package com.nhnacademy.store99.bookstore.book_image.service.impl;
 
+import com.nhnacademy.store99.bookstore.book_image.response.BookImageDTO;
 import com.nhnacademy.store99.bookstore.book_image.service.BookImageService;
 
 public class BookImageServiceImpl implements BookImageService {
+    @Override
+    public BookImageDTO getBookImageData(Long bookId) {
+        return null;
+    }
 }
