161494a12ed4cfcc2d85b1b072847296863abc7a
김승규 <rlatmdrb1996@gmail.com>
2024-04-25 13:42:25 +0900

    fix: XUserIdThreadLocal 비워지지 않는 문제 해결

--- src/main/java/com/nhnacademy/store99/bookstore/common/interceptor/XUserIdCheckInterceptor.java
+++ src/main/java/com/nhnacademy/store99/bookstore/common/interceptor/XUserIdCheckInterceptor.java
@@ -6,14 +6,13 @@ import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 public class XUserIdCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
             throws Exception {
         String xUserIdHeader = request.getHeader("X-USER-ID");
-        if(Objects.isNull(xUserIdHeader)) {
+        if (Objects.isNull(xUserIdHeader)) {
             throw new MissingUserIdHeaderException();
         }
         XUserIdThreadLocal.setXUserId(Long.parseLong(xUserIdHeader));
@@ -21,8 +20,9 @@ public class XUserIdCheckInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void postHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler,
-                           final ModelAndView modelAndView) throws Exception {
+    public void afterCompletion(final HttpServletRequest request, final HttpServletResponse response,
+                                final Object handler, final Exception ex)
+            throws Exception {
         XUserIdThreadLocal.reset();
     }
 }
