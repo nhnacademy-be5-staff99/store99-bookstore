e63cbf8b94ba988e6dd099d9a5fb8b962d741104
yrrho2 <dyrho22@naver.com>
2024-04-26 17:05:08 +0900

    refactor: 오타수정

--- src/main/java/com/nhnacademy/store99/bookstore/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book/controller/BookController.java
@@ -4,6 +4,7 @@ import com.nhnacademy.store99.bookstore.book.response.BookRequest;
 import com.nhnacademy.store99.bookstore.book_author.response.BookTransDTO;
 import com.nhnacademy.store99.bookstore.book_author.service.BookAuthorService;
 import com.nhnacademy.store99.bookstore.book_image.repository.impl.BookImageRepositoryImpl;
+import com.nhnacademy.store99.bookstore.book_image.response.BookImageDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,16 +33,11 @@ public class BookController {
 
     @GetMapping("/{bookId}")
     public BookRequest getBook(@PathVariable("bookId") Long bookId) {
-        // 만들다가 잠시 주석처리하고 커밋
-        return null;
-//        List<BookAuthorResponse> bookAuthorResponses = bookAuthorService.getAuthorBook(bookId);
-//        BookImageDTO bookRequest = bookImageRepository.getBookImageData(bookId);
-//        bookRequest.setAuthorsDTOList((List<BookRequest.AuthorDTO>) bookAuthorResponses.stream().map(a -> {
-//            return BookRequest.AuthorDTO.builder()
-//                    .AuthorName(a.getAuthorName())
-//                    .AuthorType(a.getAuthorType()).build();
-//        }));
-//        return bookRequest;
+//        return null;
+//        List<BookRequest.AuthorDTO> bookAuthorResponses = bookAuthorService.getAuthorBook(bookId);
+        BookImageDTO bookRequest = bookImageRepository.getBookImageData(bookId);
+
+        return bookRequest;
     }
 
 }
