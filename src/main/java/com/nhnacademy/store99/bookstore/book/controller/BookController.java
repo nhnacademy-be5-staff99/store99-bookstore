20c4609383b672b15b7bc8191806560df6f95629
yrrho2 <dyrho22@naver.com>
2024-04-27 15:09:46 +0900

    feat: 도서 상세보기 컨트롤러 메소드 정리

--- src/main/java/com/nhnacademy/store99/bookstore/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/store99/bookstore/book/controller/BookController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.store99.bookstore.book_author.response.BookTransDTO;
 import com.nhnacademy.store99.bookstore.book_author.service.BookAuthorService;
 import com.nhnacademy.store99.bookstore.book_image.repository.impl.BookImageRepositoryImpl;
 import com.nhnacademy.store99.bookstore.book_image.response.BookImageDTO;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -34,8 +35,15 @@ public class BookController {
     @GetMapping("/{bookId}")
     public BookRequest getBook(@PathVariable("bookId") Long bookId) {
 //        return null;
-//        List<BookRequest.AuthorDTO> bookAuthorResponses = bookAuthorService.getAuthorBook(bookId);
-        BookImageDTO bookRequest = bookImageRepository.getBookImageData(bookId);
+
+        // 도서-작가 리스트객체 받아오기
+        List<BookRequest.AuthorDTO> bookAuthorResponses = bookAuthorService.getAuthorBook(bookId);
+
+        // 도서-이미지 DTO받오기
+        BookImageDTO bookImageDTO = bookImageRepository.getBookImageData(bookId);
+
+        // BookRequest 도서 기타 정보 받아오기
+        BookRequest bookRequest = null;
 
         return bookRequest;
     }
