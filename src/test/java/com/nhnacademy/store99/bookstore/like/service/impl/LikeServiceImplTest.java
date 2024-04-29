80fe9f0504a72c7e7a793c6ded07296fd25f0a5c
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-04-29 10:38:14 +0900

    {add}: getLikeCnt api 수정

---
+++ src/test/java/com/nhnacademy/store99/bookstore/like/service/impl/LikeServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.nhnacademy.store99.bookstore.like.service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LikeServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addLike() {
+    }
+
+    @Test
+    void deleteLike() {
+    }
+
+    @Test
+    void isLiked() {
+    }
+
+    @Test
+    void countByBookId() {
+    }
+}
