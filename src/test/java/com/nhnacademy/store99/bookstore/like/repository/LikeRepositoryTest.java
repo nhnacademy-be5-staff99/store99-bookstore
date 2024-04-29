7d4faa7c232474c5c25cb2326bab33a1c7a3647c
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-04-29 11:28:31 +0900

    Delete src/test/java/com/nhnacademy/store99/bookstore/like/repository/LikeRepositoryTest.java

--- src/test/java/com/nhnacademy/store99/bookstore/like/repository/LikeRepositoryTest.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.store99.bookstore.like.repository;
-
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-public class LikeRepositoryTest {
-    
-
-}
