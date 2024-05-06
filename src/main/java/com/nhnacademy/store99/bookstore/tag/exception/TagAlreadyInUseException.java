7d60b54a3eb6c6d02de5bae9165d35f64e670651
rosin23 <rosinessrosin@gmail.com>
2024-05-06 12:49:47 +0900

    [add] : javaDoc 추가

--- src/main/java/com/nhnacademy/store99/bookstore/tag/exception/TagAlreadyInUseException.java
+++ src/main/java/com/nhnacademy/store99/bookstore/tag/exception/TagAlreadyInUseException.java
@@ -2,11 +2,6 @@ package com.nhnacademy.store99.bookstore.tag.exception;
 
 import org.springframework.http.HttpStatus;
 
-/**
- * 태그 REST 컨트롤러
- *
- * @Author rosin23
- */
 
 public class TagAlreadyInUseException extends RuntimeException{
     public TagAlreadyInUseException(HttpStatus status, String message) {
