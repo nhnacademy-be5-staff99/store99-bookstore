acbecc62dead38e51b25f14e26fe3ae09ed7721b
yrrho2 <dyrho22@naver.com>
2024-05-07 16:39:04 +0900

    comment: query작성

--- src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/repository/OrderBookRepository.java
+++ src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/repository/OrderBookRepository.java
@@ -1,4 +1,13 @@
 package com.nhnacademy.store99.bookstore.order_book.entity.repository;
 
 public interface OrderBookRepository {
+
+    // 이 쿼리는 각 도서의 주문갯수를 가져옴. 그리고 가장 많이 팔리는걸 처음으로 가져옴.
+    // select ob.book_id, COUNT(ob.book_id)  from order_book ob GROUP BY ob.book_id  Order By COUNT(ob.book_id) DESC;
+
+
+    // 이거는 최신 도서를 가져옴
+    // select b.book_id, b.created_at  from books b ORDER BY b.created_at DESC, b.book_id ;
 }
+
+
