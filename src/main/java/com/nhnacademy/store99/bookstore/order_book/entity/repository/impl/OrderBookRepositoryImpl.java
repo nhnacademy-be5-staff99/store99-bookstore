d1299dc01523638060445a590648923bcd1c5b97
yrrho2 <dyrho22@naver.com>
2024-05-08 14:26:28 +0900

    feat: 주문 개수 정렬 도서목록

--- src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/repository/impl/OrderBookRepositoryImpl.java
+++ src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/repository/impl/OrderBookRepositoryImpl.java
@@ -1,16 +1,35 @@
 package com.nhnacademy.store99.bookstore.order_book.entity.repository.impl;
 
+import com.nhnacademy.store99.bookstore.book.entity.QBook;
+import com.nhnacademy.store99.bookstore.order_book.entity.OrderBook;
+import com.nhnacademy.store99.bookstore.order_book.entity.QOrderBook;
 import com.nhnacademy.store99.bookstore.order_book.entity.repository.OrderBookRepository;
-import com.nhnacademy.store99.bookstore.order_book.entity.response.BestBookResponse;
 import com.nhnacademy.store99.bookstore.order_book.entity.response.LatestBookResponse;
+import com.querydsl.core.group.GroupBy;
 import java.util.List;
+import java.util.Map;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
 
-public class OrderBookRepositoryImpl implements OrderBookRepository {
-    @Override
-    public List<BestBookResponse> bestBooks() {
-        return null;
+@Repository
+public class OrderBookRepositoryImpl extends QuerydslRepositorySupport implements OrderBookRepository {
+    public OrderBookRepositoryImpl() {
+        super(OrderBook.class);
     }
 
+    @Override
+    public Map<Long, Long> bestBooks() {
+        QBook book = QBook.book;
+        QOrderBook orderBook = QOrderBook.orderBook;
+
+        return from(orderBook)
+                .groupBy(orderBook.book.id)
+                .orderBy(orderBook.book.id.count().desc())
+                .transform(GroupBy.groupBy(orderBook.book.id).as(orderBook.book.id.count()));
+    }
+
+
+    // select b.book_id, b.created_at  from books b ORDER BY b.created_at DESC, b.book_id DESC;
     @Override
     public List<LatestBookResponse> latestBooks() {
         return null;
--- src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/service/impl/OrderBookServiceImpl.java
+++ src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/service/impl/OrderBookServiceImpl.java
@@ -1,14 +1,21 @@
 package com.nhnacademy.store99.bookstore.order_book.entity.service.impl;
 
-import com.nhnacademy.store99.bookstore.order_book.entity.response.BestBookResponse;
+import com.nhnacademy.store99.bookstore.order_book.entity.repository.OrderBookRepository;
 import com.nhnacademy.store99.bookstore.order_book.entity.response.LatestBookResponse;
 import com.nhnacademy.store99.bookstore.order_book.entity.service.OrderBookService;
 import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class OrderBookServiceImpl implements OrderBookService {
+    private final OrderBookRepository orderBookRepository;
+
     @Override
-    public List<BestBookResponse> bestBooks() {
-        return null;
+    public Map<Long, Long> bestBooks() {
+        return orderBookRepository.bestBooks();
     }
 
     @Override
