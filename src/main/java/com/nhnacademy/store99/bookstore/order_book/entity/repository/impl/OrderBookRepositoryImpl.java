c55597a82356ec7368971be8737fb71a8e74073e
yrrho2 <dyrho22@naver.com>
2024-05-08 10:26:19 +0900

    add: 메소드추가

--- src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/repository/impl/OrderBookRepositoryImpl.java
+++ src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/repository/impl/OrderBookRepositoryImpl.java
@@ -1,4 +1,18 @@
 package com.nhnacademy.store99.bookstore.order_book.entity.repository.impl;
 
-public class OrderBookRepositoryImpl {
+import com.nhnacademy.store99.bookstore.order_book.entity.repository.OrderBookRepository;
+import com.nhnacademy.store99.bookstore.order_book.entity.response.BestBookResponse;
+import com.nhnacademy.store99.bookstore.order_book.entity.response.LatestBookResponse;
+import java.util.List;
+
+public class OrderBookRepositoryImpl implements OrderBookRepository {
+    @Override
+    public List<BestBookResponse> bestBooks() {
+        return null;
+    }
+
+    @Override
+    public List<LatestBookResponse> latestBooks() {
+        return null;
+    }
 }
--- src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/service/impl/OrderBookServiceImpl.java
+++ src/main/java/com/nhnacademy/store99/bookstore/order_book/entity/service/impl/OrderBookServiceImpl.java
@@ -1,4 +1,18 @@
 package com.nhnacademy.store99.bookstore.order_book.entity.service.impl;
 
-public class OrderBookServiceImpl {
+import com.nhnacademy.store99.bookstore.order_book.entity.response.BestBookResponse;
+import com.nhnacademy.store99.bookstore.order_book.entity.response.LatestBookResponse;
+import com.nhnacademy.store99.bookstore.order_book.entity.service.OrderBookService;
+import java.util.List;
+
+public class OrderBookServiceImpl implements OrderBookService {
+    @Override
+    public List<BestBookResponse> bestBooks() {
+        return null;
+    }
+
+    @Override
+    public List<LatestBookResponse> latestBooks() {
+        return null;
+    }
 }
