e8549e483bd29e0c461b069fa774b4d9da9acf28
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-05-09 16:23:48 +0900

    {add}: 좋아요 수정 및 마이페이지 조회 추

--- src/main/java/com/nhnacademy/store99/bookstore/like/repository/LikeRepositoryCustom.java
+++ src/main/java/com/nhnacademy/store99/bookstore/like/repository/LikeRepositoryCustom.java
@@ -7,6 +7,16 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface LikeRepositoryCustom {
 
-    List<BookInfoForLikeResponse> getAllByUser(Long userId);
+    /**
+     * 도서 별 좋아요 수를 조회하는 메소드 입니다.
+     *
+     * @param bookId
+     * @return SELECT COUNT(*)
+     * FROM like AS l
+     * JOIN book AS b
+     * ON l.book_id = b.book_id;
+     */
+    Long countAllByBook(Long bookId);
 
+    List<BookInfoForLikeResponse> getAllByUser(Long userId);
 }
