0ee30bc3e9d71a41b9fede88cec4e03a847e68ad
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2024-05-09 16:23:48 +0900

    {add}: 좋아요 수정 및 마이페이지 조회 추

--- src/main/java/com/nhnacademy/store99/bookstore/like/service/LikeQueryService.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.store99.bookstore.like.service;
-
-import com.nhnacademy.store99.bookstore.like.dto.response.BookInfoForLikeResponse;
-import org.springframework.data.domain.Page;
-
-public interface LikeQueryService {
-    Page<BookInfoForLikeResponse> getAllByUser();
-}
