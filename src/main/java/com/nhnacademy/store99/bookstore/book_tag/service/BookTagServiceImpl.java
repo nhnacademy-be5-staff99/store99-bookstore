5aa7148328d9881667d2a8e89a6f1bc2176a4a31
rosin23 <rosinessrosin@gmail.com>
2024-05-09 15:18:45 +0900

    feat : bookId로 bookTag와 연관된 book 테이블과 tag 테이블에서 해당 bookId의 tagName 리스트 반환

---
+++ src/main/java/com/nhnacademy/store99/bookstore/book_tag/service/BookTagServiceImpl.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.store99.bookstore.book_tag.service;
+
+import com.nhnacademy.store99.bookstore.book.response.BookResponse;
+import com.nhnacademy.store99.bookstore.book_tag.repository.BookTagRepositoryCustomImpl;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 도서 - 태그 참조 기능을 제공하는 서비스 구현체
+ *
+ * @author rosin23
+ */
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BookTagServiceImpl implements BookTagService {
+
+    @Qualifier("bookTagRepositoryCustomImpl")
+    private final BookTagRepositoryCustomImpl bookTagRepositoryCustom;
+
+    /**
+     * book id를 이용한 태그 검색
+     *
+     * @param bookId
+     * @return tag id, book id, tag name을 반환
+     */
+
+    @Override
+    public List<BookResponse.TagDTO> getTagByBookId(Long bookId) {
+        return bookTagRepositoryCustom.findByBookId(bookId);
+    }
+}
