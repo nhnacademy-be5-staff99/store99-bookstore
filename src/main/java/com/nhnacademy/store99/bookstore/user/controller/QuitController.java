4ef8dc2459f8aba1b9140e9cab14fc9b467252fc
jinhyogyeom <jdkk1541@naver.com>
2024-05-15 19:03:10 +0900

    -회원 탈퇴 시 deleted_at 값 업데이트 -탈퇴한 이메일 검증

---
+++ src/main/java/com/nhnacademy/store99/bookstore/user/controller/QuitController.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.store99.bookstore.user.controller;
+
+import com.nhnacademy.store99.bookstore.user.service.QuitService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users")
+
+public class QuitController {
+    private final QuitService quitService;
+
+    public QuitController(QuitService quitService) {
+        this.quitService = quitService;
+    }
+
+    @PostMapping("/quit")
+    public Long quit(@RequestHeader("X-USER-ID") Long xUserId) {
+        quitService.quit(xUserId);
+        return xUserId;
+    }
+}
